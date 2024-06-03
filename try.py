31c5691c889f4c0136627f6713ebeb28427d94b5
artkovl <artkoval@gmail.com>
2024-06-03 15:45:59 +0300

    final ver. 2.2 (w.plotly)

--- try.py
+++ try.py
@@ -18,75 +18,80 @@ def open_data():
     return df_sel
 
 
-st.set_page_config(
-    page_title='Продажи жилья в Москве',
-    page_icon=':bar_chart:',
-    layout='wide',
-    initial_sidebar_state="auto",
-)
+def main():
+    st.set_page_config(
+        page_title='Продажи жилья в Москве',
+        page_icon=':bar_chart:',
+        layout='wide',
+        initial_sidebar_state="auto",
+    )
 
-col1, col2, col3 = st.columns([5, 1, 15])
+    col1, col2, col3 = st.columns([5, 1, 15])
 
-df = open_data()
-sub_area_list = df['sub_area'].unique().tolist()
-sub_area_list_sorted = sorted(sub_area_list)
-rooms_list = df['num_room'].unique().tolist()
-rooms_list_sorted = sorted(rooms_list)
-price_doc_list = df['price_doc'].unique().tolist()
+    df = open_data()
+    sub_area_list = df['sub_area'].unique().tolist()
+    sub_area_list_sorted = sorted(sub_area_list)
+    rooms_list = df['num_room'].unique().tolist()
+    rooms_list_sorted = sorted(rooms_list)
+    price_doc_list = df['price_doc'].unique().tolist()
 
-col3.title('Продажи жилья в Москве')
-col3.markdown('Данные с августа 2011 по июнь 2015 года')
+    col3.title('Продажи жилья в Москве')
+    col3.markdown('Данные с августа 2011 по июнь 2015 года')
 
-col1.image(Image.open(f'data/{choice(range(1, 7))}.jpg'))
-col1.markdown('')
-col1.markdown('Выбор параметров:')
-sub_area_choice = col1.multiselect('Районы:', sub_area_list_sorted, default=None)
-build_year_choice = col1.number_input('Год постройки от:', value=0, step=1)
-rooms_list_choice = col1.multiselect('Количество комнат:', rooms_list_sorted, default=None)
-floor_choice = col1.slider('Диапазон этажей:', value=[0, 77])
-price_choice = col1.slider('Диапазон цен, руб.', value=[min(price_doc_list), max(price_doc_list)])
+    col1.image(Image.open(f'data/{choice(range(1, 7))}.jpg'))
+    col1.markdown('')
+    col1.markdown('Выбор параметров:')
+    sub_area_choice = col1.multiselect('Районы:', sub_area_list_sorted, default=None)
+    build_year_choice = col1.number_input('Год постройки от:', value=0, step=1)
+    rooms_list_choice = col1.multiselect('Количество комнат:', rooms_list_sorted, default=None)
+    floor_choice = col1.slider('Диапазон этажей:', value=[0, 77])
+    price_choice = col1.slider('Диапазон цен, руб.', value=[min(price_doc_list), max(price_doc_list)])
 
-df = df.loc[
-    (df['num_room'].isin(rooms_list_choice)) &
-    (df['floor'] >= floor_choice[0]) &
-    (df['floor'] <= floor_choice[1]) &
-    (df['build_year'] >= build_year_choice) &
-    (df['sub_area'].isin(sub_area_choice)) &
-    (df['price_doc'] > price_choice[0]) &
-    (df['price_doc'] < price_choice[1])
-    ]
+    df = df.loc[
+        (df['num_room'].isin(rooms_list_choice)) &
+        (df['floor'] >= floor_choice[0]) &
+        (df['floor'] <= floor_choice[1]) &
+        (df['build_year'] >= build_year_choice) &
+        (df['sub_area'].isin(sub_area_choice)) &
+        (df['price_doc'] > price_choice[0]) &
+        (df['price_doc'] < price_choice[1])
+        ]
 
-# with st.expander('Data preview'):
-col3.dataframe(
-    df,
-    column_config={
-        'build_year': st.column_config.NumberColumn(format="%d")
-    },
-)
+    # with st.expander('Data preview'):
+    col3.dataframe(
+        df,
+        column_config={
+            'build_year': st.column_config.NumberColumn(format="%d")
+        },
+    )
 
-fig = px.scatter(df, x='timestamp', y='price_doc', size='full_sq', symbol='num_room', color='sub_area',
-                 hover_data=['life_sq', 'floor', 'max_floor', 'build_year', 'kitch_sq', 'metro_km_walk'])
+    fig = px.scatter(df, x='timestamp', y='price_doc', size='full_sq', symbol='num_room', color='sub_area',
+                     hover_data=['life_sq', 'floor', 'max_floor', 'build_year', 'kitch_sq', 'metro_km_walk'])
 
-col3.subheader('Диаграмма продаж жилья в Москве')
-col3.plotly_chart(fig)
+    col3.subheader('Диаграмма продаж жилья в Москве')
+    col3.plotly_chart(fig)
 
-# user axis selection
-x_axis = col3.selectbox('Выберите показатель для оси X', df.columns, index=0)
-y_axis = col3.selectbox('Выберите показатель для оси Y', df.columns, index=1)
+    # user axis selection
+    x_axis = col3.selectbox('Выберите показатель для оси X', df.columns, index=0)
+    y_axis = col3.selectbox('Выберите показатель для оси Y', df.columns, index=1)
 
-# create chart with selectable axis
-fig_sel = px.scatter(df, x=x_axis, y=y_axis, size='full_sq', symbol='num_room', color='sub_area',
-                     hover_data=['full_sq', 'life_sq', 'floor', 'max_floor', 'build_year', 'kitch_sq',
-                                 'metro_km_walk'])
+    # create chart with selectable axis
+    fig_sel = px.scatter(df, x=x_axis, y=y_axis, size='full_sq', symbol='num_room', color='sub_area',
+                         hover_data=['full_sq', 'life_sq', 'floor', 'max_floor', 'build_year', 'kitch_sq',
+                                     'metro_km_walk'])
 
-col3.subheader('Диаграмма продаж жилья в Москве (с выбором категорий)')
+    col3.subheader('Диаграмма продаж жилья в Москве (с выбором категорий)')
 
-fig_sel.update_layout(
-    font_family='Times New Roman',
-    title='Продажи жилья в Москве',
-    xaxis_title=x_axis,
-    yaxis_title=y_axis,
-    legend_title='Район'
-)
+    fig_sel.update_layout(
+        font_family='Times New Roman',
+        title='Продажи жилья в Москве',
+        xaxis_title=x_axis,
+        yaxis_title=y_axis,
+        legend_title='Район'
+    )
 
-col3.plotly_chart(fig_sel)
+    col3.plotly_chart(fig_sel)
+
+
+if __name__ == '__main__':
+    main()
